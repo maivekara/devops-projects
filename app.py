102fbe8c603f125e8a9f353fef7a0f6b3a5fbbc5
Sefa Öztürk <osefa3435@gmail.com>
2025-07-03 12:58:03 +0300

    feat: yeni api/status endpoint eklendi

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Merhaba DevOps Dunyasi! CI/CD Pipeline Calisiyor! 🚀"
+@app.route('/api/status')
+def status():
+    return jsonify({"status": "OK", "message": "API çalışıyor! 🚀"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
