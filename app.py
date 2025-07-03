bb2f0576d2a1151d63266234951f8433457470e3
Sefa Öztürk <osefa3435@gmail.com>
2025-07-03 12:43:22 +0300

    test: hata denemesi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Merhaba DevOps Dunyasi! CI/CD Pipeline Calisiyor! 🚀"
+    reurn "Merhaba DevOps Dunyasi! CI/CD Pipeline Calisiyor! 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
